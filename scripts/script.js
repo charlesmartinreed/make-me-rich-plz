6fd4fb6dd07a511e86bfa69884d9cebc932ca271
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-11-05 23:55:40 -0500

    Refactor and add lotto type selector.

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,67 @@
 let numberEl = document.querySelector("#numbers-container");
+let lottoTypeSelector = document.querySelector("#lotto-type-select");
 
 const LottoTypes = {
-  powerball: { minVal: 1, maxVal: 69, count: 6 },
+  powerball: {
+    whiteBallsNumRange: { minVal: 1, maxVal: 69 },
+    coloredBallNumRange: { minVal: 1, maxVal: 26, ballColor: "red" },
+    count: 6,
+  },
+  megamillions: {
+    whiteBallsNumRange: { minVal: 1, maxVal: 70 },
+    coloredBallNumRange: { minVal: 1, maxVal: 25, ballColor: "gold" },
+    count: 6,
+  },
 };
 
+let currentLottoType = "powerball";
+
 function generateNumbers(lottoType) {
-  let { minVal, maxVal, count } = lottoType;
+  clearNumbers();
 
-  let lottoNumbers = Array(count)
-    .fill(0, 0)
-    .map(
-      (oldValue) =>
-        (oldValue = Math.floor(Math.random() * (maxVal - minVal) - minVal))
-    );
+  let {
+    whiteBallsNumRange: { minVal: whiteMinVal, maxVal: whiteMaxVal },
+    coloredBallNumRange: {
+      minVal: colorMinVal,
+      maxVal: colorMaxVal,
+      ballColor,
+    },
+    count,
+  } = lottoType;
 
-  console.log(lottoNumbers);
+  let lottoNumbers = [
+    ...createNumberArr(whiteMinVal, whiteMaxVal, count - 1),
+    ...createNumberArr(colorMinVal, colorMaxVal, 1),
+  ];
 
+  function createNumberArr(minNum, maxNum, arrLen) {
+    // using Math.ceil as a quick, dirty way of preventing -1 in the event that both max and min randomize to 1
+    return Array(arrLen)
+      .fill(0, 0)
+      .map(
+        (emptyVal) =>
+          (emptyVal = Math.ceil(Math.random() * (maxNum - minNum) - minNum))
+      );
+  }
+
+  layoutNumbers(lottoNumbers);
+}
+
+function layoutNumbers(lottoNumbers) {
   for (const number of lottoNumbers) {
     numberEl.innerHTML += `<span class="number-result">${number}</span>`;
   }
 }
 
-generateNumbers(LottoTypes.powerball);
+function clearNumbers() {
+  numberEl.innerHTML = "";
+}
+
+lottoTypeSelector.addEventListener("change", (e) => {
+  currentLottoType = e.target.value;
+  generateNumbers(LottoTypes[currentLottoType]);
+});
+
+window.addEventListener("DOMContentLoaded", (e) =>
+  generateNumbers(LottoTypes[currentLottoType])
+);
